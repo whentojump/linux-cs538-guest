ed4c8a3f9f652144be84cc20d3d31c2dd47f93f8
Wentao Zhang <zhangwt1997@gmail.com>
2025-11-18 22:33:37 -0600

    fix util

--- utils/sorted-per-site-stats.sh
+++ utils/sorted-per-site-stats.sh
@@ -2,8 +2,12 @@
 
 # Sort by the first column (skbuff->head) using an external script
 
-f=/proc/netmem_stats/per_site
+f=$(mktemp)
 
-head -3 $f
-tail -n+4 $f | head -n-1 | sort
+cat /proc/netmem_stats/per_site > $f
+
+head -2 $f
+tail -n+3 $f | head -n-1 | sort
 tail -1 $f
+
+rm -f $f
