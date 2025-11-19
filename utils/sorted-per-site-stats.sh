e9d37cafc596f28033f504108048df284e9c4312
Wentao Zhang <zhangwt1997@gmail.com>
2025-11-19 00:39:21 -0600

    workloads and helper script

---
+++ utils/sorted-per-site-stats.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Sort by the first column (skbuff->head) using an external script
+
+f=$(mktemp)
+
+cat /proc/netmem_stats/per_site > $f
+
+head -2 $f
+tail -n+3 $f | head -n-1 | sort
+tail -1 $f
+
+rm -f $f
