0298a223c8d4d4633a93ffc8e37cc52de5a6f6fb
Wentao Zhang <zhangwt1997@gmail.com>
2025-11-19 18:38:38 -0600

    suppress ksize issues

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -1308,8 +1308,19 @@ void kfree_sensitive(const void *p)
 }
 EXPORT_SYMBOL(kfree_sensitive);
 
+#include <net/netmem_pool.h>
+
 size_t ksize(const void *objp)
 {
+	struct netmem_alloc_header *header;
+	const void *real_ptr;
+
+	real_ptr = objp - sizeof(struct netmem_alloc_header);
+	header = (struct netmem_alloc_header *)real_ptr;
+	if (header->magic == NETMEM_FROM_POOL || header->magic == NETMEM_FROM_KMALLOC) {
+		return header->size;
+	}
+
 	/*
 	 * We need to first check that the pointer to the object is valid.
 	 * The KASAN report printed from ksize() is more useful, then when
